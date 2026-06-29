c549b0da026666c1cd679edfd972733435e8bb02
pkexcellent <pengkangexcellent@163.com>
2026-06-28 21:24:01 -0400

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.go
@@ -0,0 +1,15 @@
+func uniquePaths(m int, n int) int {
+    // use math method
+	// pick n-1 from m-1+n-1
+	// C n-1+m-1 m-1
+	// ((n-1+m-1)*....*(n-1+m-1-n+1))/ (n*(n-1)*...*1)
+	// m*(m-1)*....*(m-n+1) / n!, (m - (m-n+1) + 1) = n. n-1+1 = n
+	// C 5 2 = 5*4/2*1
+	// C 5 3 = 5*4*3/3*2*1
+	res := 1
+	for i, j := (n-1)+(m-1), 1; i >= (n-1)+(m-1)-(n-1)+1; i, j = i-1, j+1 {
+		res = res*i/j
+	}
+
+	return res
+}
