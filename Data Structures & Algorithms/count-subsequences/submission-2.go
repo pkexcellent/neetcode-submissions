f74ac7e33d9d30bbe5c5732d378bf8fdfea79cbe
pkexcellent <pengkangexcellent@163.com>
2026-07-10 15:46:11 -0700

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.go
@@ -0,0 +1,55 @@
+func numDistinct(s string, t string) int {
+    // build a 2D metrix to show [i][j] i is the ith letter in t, j is the jth letter
+    // value is 1 means hit
+    /*
+cat, caaat
+
+1 0 0 0 0
+0 1 1 1 0
+0 0 0 0 1
+
+1 0 0 0 0
+0 1 1 1 0
+0 0 0 0 3
+
+
+s="xxyxy"
+t="xy"
+[
+    [1 1 0 1 0] 
+    [0 0 1 0 1]
+]
+[
+    [1 1 0 1 0] 
+    [0 0 2 0 3]
+]
+    */
+    r, c := len(t), len(s)
+    dp := make([][]int, r)
+    for i, _ := range dp {
+        dp[i] = make([]int, c)
+    }
+    for i := 0; i < r; i++ {
+        target := t[i]
+        for j := 0; j < c; j++ {
+            if s[j] == target {
+                dp[i][j] = 1
+            }
+        }
+    }
+    for i := 1; i < r; i++ {
+        for j := 0; j < c; j++ {
+            if dp[i][j] != 0 {
+                dp[i][j] = 0 // reassign it to 0, it should be decided by the previous row
+                for k := 0; k < j; k++ {
+                    dp[i][j] += dp[i-1][k]
+                }
+            }   
+        }
+    }
+    rs := 0
+    for i := 0; i < c; i++ {
+        rs += dp[r-1][i]
+    }
+    return rs
+}
