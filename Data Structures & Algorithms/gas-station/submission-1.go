de26bb92f0c5496ae943b5ae0403adb0296f288b
pkexcellent <pengkangexcellent@163.com>
2026-07-12 23:06:17 -0400

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.go
@@ -0,0 +1,43 @@
+func canCompleteCircuit(gas []int, cost []int) int {
+    // this is like the sub array with always positive sum
+    n := len(gas)
+    remains := make([]int, n)
+    for i := 0; i < n; i++ {
+        remains[i] = gas[i] - cost[i]
+    }
+    // now remains means the each delta for stations, 
+    // we need to start with a positive one, and while adding, the sum should always be positive
+    startPos := -1
+    for i := 0; i < n; i++ {
+        if remains[i] < 0 {
+            continue
+        }
+        streakSum := 0 // start loop
+        canDo := true
+        for j := i; j <= i-1+n; j++ { // loop a circle, the next round j is (i-1)
+            //fmt.Println("pos: ", i, j, j%n)
+            streakSum += remains[j%n]
+            if streakSum < 0 {
+                canDo = false
+                break
+            }
+        }
+        /*
+        if !canDo {
+            continue
+        }
+        for j := 0; j < i; j++ {
+            streakSum += remains[j]
+            if streakSum < 0 {
+                canDo = false
+                break
+            }
+        }
+        */
+        if canDo {
+            startPos = i
+            break
+        }  
+    }
+    return startPos
+}
