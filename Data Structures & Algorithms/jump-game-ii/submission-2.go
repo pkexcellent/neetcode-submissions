59cec22dd5e05c546decfab8c3819619d02c0509
pkexcellent <pengkangexcellent@163.com>
2026-07-12 22:04:31 -0400

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.go
@@ -0,0 +1,20 @@
+func jump(nums []int) int {
+    // use dp[i] to store how many the min steps can reach i
+    // repeatly update dp[i]
+    n := len(nums)
+    dp := make([]int, len(nums))
+    for i := 0; i < n; i++ {
+        dp[i] = math.MaxInt32
+    }
+    dp[0] = 0
+    for i := 0; i < n; i++ {
+        longest := i+nums[i]
+        minStepAti := dp[i]
+        for j := i+1; j < min(longest+1, n); j++ {
+            dp[j] = min(dp[j], minStepAti+1) // i also means the ith jump
+        }
+        //fmt.Println(dp)
+    }
+    
+    return dp[n-1]
+}
