b5df329939a144420fd75d43582864aae167301d
pkexcellent <pengkangexcellent@163.com>
2026-07-19 23:10:04 -0400

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.go
@@ -0,0 +1,19 @@
+func getSum(a int, b int) int {
+	// use ^ without carry  <- sumNoCarry
+	// use & << 1 to get carry <- carry
+	// check if carry is 0, is 0 means stopping loop. 
+	// then 
+
+	for b != 0 {
+		carry := (a & b) << 1
+		a = a ^ b
+		b = carry
+		//fmt.Println(a, b, carry)
+	}
+
+	if a <= math.MaxInt {
+		return a
+	}
+	return ^(a^0xFFFFFFFF)
+	
+}
