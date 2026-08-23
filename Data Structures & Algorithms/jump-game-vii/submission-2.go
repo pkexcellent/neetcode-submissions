3fa3f5b37ace26f4dde32ce359134b7f1ded99c0
pkexcellent <pengkangexcellent@163.com>
2026-08-23 15:59:38 -0400

    Add: jump-game-vii - submission-2

---
+++ Data Structures & Algorithms/jump-game-vii/submission-2.go
@@ -0,0 +1,23 @@
+func canReach(s string, minJump int, maxJump int) bool {
+	// dp
+	n := len(s)
+	if s[n-1] != '0' {return false}
+
+	dp := make([]bool, n)
+	dp[0] = true
+	j := 0
+	for i := 0; i < n; i++ {
+		if dp[i] != true {continue}
+		// the optimized method is to avoid re-calculate j
+		j = max(j, i+minJump)
+		for ; j <= i + maxJump && j < n; j++ {
+			if s[j] == '0' {
+				dp[j] = true
+			}
+			if j == n-1 && dp[j] {
+				return true
+			}
+		}
+	}
+	return dp[n-1]
+}
